64e9490ae0f75d36f8f9a6615f9c27d26db18af9
Richard Badir <ricobadir@hotmail.com>
2024-01-27 17:08:12 -0500

    Trimming transcript to get clip of different people works

--- speech_recoginition/speaker_recoginition.py
+++ speech_recoginition/speaker_recoginition.py
@@ -20,9 +20,13 @@ def create_audio(count, bitrate="192k"):
 
     if not os.path.exists(audio_directory):
         os.makedirs(audio_directory)
-    write(f"{audio_directory}\\user_{count}.wav", freq, recording)
+    name =f"user_{count}.wav"
+    if count<0:
+        name = "transcript.wav"
 
-    wv.write(f"{audio_directory}\\user_{count}.wav", recording, freq, sampwidth=2)
+    write(f"{audio_directory}\\{name}", freq, recording)
+
+    wv.write(f"{audio_directory}\\{name}", recording, freq, sampwidth=2)
 
 def diaritize(path):
     falcon = pvfalcon.create(access_key=access_key)
@@ -33,15 +37,21 @@ def diaritize(path):
             "{speaker_tag=%d start_sec=%.2f end_sec=%.2f}"
             % (segment.speaker_tag, segment.start_sec, segment.end_sec)
         )
+    return segments
 def trim_audio(input_path, count, start_ms, end_ms):
     audio = AudioSegment.from_wav(input_path)
     trimmed_audio = audio[start_ms:end_ms]
     trimmed_audio.export(f"{audio_directory}\\trim_{count}.wav", format="wav")
 
 
-create_audio(1)
+create_audio(-1)
 print("someone else talk")
 #create_audio(2)
-trim_audio(f"{audio_directory}\\user_1.wav",1,0, 2500)
-#diaritize(f"{audio_directory}\\user_1.wav")
+#trim_audio(f"{audio_directory}\\user_1.wav",1,0, 2500)
+segments=diaritize(f"{audio_directory}\\transcript.wav")
+speakers= set()
+for segment in segments:
+    if segment.speaker_tag not in speakers:
+        speakers.add(segment.speaker_tag)
+        trim_audio(f"{audio_directory}\\transcript.wav",segment.speaker_tag, segment.start_sec,segment.end_sec)
 
